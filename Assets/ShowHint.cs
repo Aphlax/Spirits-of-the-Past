e54cc5db5b8172d3af5e4d36031bb67e512668f5
Fabian Meier <Aphlax@users.noreply.github.com>
2023-02-04 22:19:50 +0100

    Added City level.

---
+++ Assets/ShowHint.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ShowHint : MonoBehaviour
+{
+
+    public GameObject panel;
+    public GameObject player;
+    public float distance;
+
+    
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        panel.active = Vector2.Distance(transform.position, player.transform.position) < distance;
+    }
+}
